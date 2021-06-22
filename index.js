b1c3e6cf9f666661bbd828b50310169bc929c4aa
sejaljannu <sejaljannu@gmail.com>
2021-06-21 20:01:26 -0500

    Homework 19
    Homework 19 includes the JQuery event listener.

---
+++ index.js
@@ -0,0 +1,11 @@
+$('#learnmore').on("click", function(){
+    $('.hideText').slideToggle();
+  });
+
+$('#learnmore2').on("click", function(){
+    $('.hideText2').slideToggle();
+  });
+
+  $('#learnmore3').on("click", function(){
+    $('.hideText3').slideToggle();
+  });
